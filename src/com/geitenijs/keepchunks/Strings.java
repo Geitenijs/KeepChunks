8f32b7d9e5150a67662f40bf35ec7e6096e71925
BuildTools <unconfigured@null.spigotmc.org>
2023-11-30 20:26:47 +0100

    bump v

--- src/com/geitenijs/keepchunks/Strings.java
+++ src/com/geitenijs/keepchunks/Strings.java
@@ -6,7 +6,7 @@ public class Strings {
     static final String INTERNALPREFIX = "[KeepChunks] ";
     public static final String DEBUGPREFIX = "[DEBUG] ";
     public static final String GAMEPREFIX = "&2Keep&8Chunks &7// ";
-    public static final String VERSION = "1.6.10";
+    public static final String VERSION = "1.6.11";
     public static final String AUTHOR = "Geitenijs";
     static final int RESOURCEID = 23307;
     static final String WEBSITE = "https://www.spigotmc.org/resources/" + Strings.RESOURCEID;
